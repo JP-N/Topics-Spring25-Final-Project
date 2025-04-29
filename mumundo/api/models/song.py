4aa68ca9bd371f08fc0cd828a134ff8b00e5b56f
kpiaszynski <kpiaszynski@uiowa.edu>
2025-04-28 23:14:36 -0500

    Update song.py

--- mumundo/api/models/song.py
+++ mumundo/api/models/song.py
@@ -4,11 +4,12 @@ from pydantic import BaseModel
 
 class Song(Document):
     Spotify_id: int
+    Title: str
+    Artist: str
+    Length: int # in seconds
     Likes: int = 0
     Dislikes: int = 0
     Playlist_count: int = 0
-    Genre: str | None
-    Length: int # in seconds
 
     def rating(self):
         return self.likes / max(self.dislikes, 1)
