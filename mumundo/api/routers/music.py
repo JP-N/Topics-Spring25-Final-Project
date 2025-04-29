8141d915cc2f27a6c6deaa328eb7043db4c38cd2
kpiaszynski <kpiaszynski@uiowa.edu>
2025-04-29 00:39:00 -0500

    Update music.py for mongodb compatibility, added search with spotify api (needs tweaking)

--- mumundo/api/routers/music.py
+++ mumundo/api/routers/music.py
@@ -1,57 +1,79 @@
-from fastapi import APIRouter, Path, HTTPException, status
+from fastapi import APIRouter, Path, HTTPException, status, Depends, Body
 from pydantic import BaseModel
 from typing import List
+import requests
+
+from mumundo.models.song import SongRequest, Song
 
 music_router = APIRouter()
 
-class MusicItem(BaseModel):
-    id: int
-    title: str
-    artist: str
-    description: str = ""
 
-class MusicItemRequest(BaseModel):
-    title: str 
-    artist: str 
-    description: str = ""
+@music_router.get("/search")
+def fetch_Spotify(query: str):
+    headers = {"Authorization": f"Bearer {SPOTIFYTOKEN}"}
+    params = {"q": query, "type": "track", "limit": 10}
+    response = requests.get("https://api.spotify.com/v1/search", headers=headers, params=params)
+    items = response.json().get("tracks", {}).get("items", [])
 
-music_list = []
-max_id: int = 0
+    return[{
+        "spotify_id": i["id"],
+            "title": i["name"],
+            "artist": ", ".join([artist["name"] for artist in i["artists"]]),
+            "length": i["duration_ms"] // 1000,
+            "album": i["album"]["name"]
+    }for i in items
+    ]
 
-#create a new music item
-@music_router.post("", status_code = status.HTTP_201_CREATED, response_model = MusicItem)
-def add_music(item: MusicItemRequest): #accepts request model without id
-    global max_id
-    max_id += 1
-    new_item = MusicItem(id = max_id, title = item.title, artist = item.artist, description = item.description)
-    music_list.append(new_item)
-    return new_item
 
-#reads all music items
-@music_router.get("", response_model = List[MusicItem])
-def get_music():
-    return music_list
+@music_router.post("/", response_model= Song)
+async def add_music(request: SongRequest):
+    headers = {"Authorization": f"Bearer {SPOTIFYTOKEN}"}
+    response = requests.get(f"https://api.spotify.com/v1/tracks/{request.spotify_id}",headers = headers)
+    if response.status_code != 200:
+        raise HTTPException(404, "Song not found on Spotify.")
+    track = response.json()
 
-#gets a specific music item by id
-@music_router.get("/{id}", response_model = MusicItem)
-def get_music_by_id(id: int = Path(..., title= "Music ID")):
-    for item in music_list:
-        if item.id == id:
-            return item
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Music item with ID={id} not found.")
+    song = Song(
+        spotify_id=track["id"],
+        title=track["name"],
+        artist=", ".join([i["name"] for i in track["artists"]]),
+        length=track["duration_ms"] // 1000,
+        image_url=track["album"]["images"][0]["url"]
+    )
+    await song.insert()
+    return song
 
-#updates a music item
-@music_router.put("/{id}", response_model= MusicItem)
-def update_music(id: int, updated_item: MusicItemRequest):
-    for index, item in enumerate(music_list):
-        if item.id == id:
-            music_list[index] = MusicItem(id=id, **updated_item.dict())
-            return music_list[index]
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Music item with ID={id} not found.")
+@music_router.get("/{id}", response_model=Song)
+async def get_song(id: str):
+    song = await Song.get(id)
+    if not song:
+        raise HTTPException(404, "Song not found")
+    return song
 
-#deletes a music item
+@music_router.get("", response_model = List[Song])
+async def get_all_songs():
+    songs = await Song.find_all().to_list()
+    return songs
+
 @music_router.delete("/{id}")
-def delete_music(id: int):
-    global music_list
-    music_list = [item for item in music_list if item.id != id]
-    return {"message": f"Music item with ID={id} has been deleted."}
+async def delete_song(id:str):
+    song = await Song.get(id)
+    if not song:
+        raise HTTPException(404, "Song not found")
+    await song.delete()
+    return {"message": "Song deleted."}
+
+ALLOWED_FIELDS = {"Image_url"}
+
+@music_router.put("/{id}", response_model = Song)
+async def update_song(id:str, data:dict):
+    song = await Song.get(id)
+    if not song:
+        raise HTTPException(404, "Song not found")
+    for field, value in data.items():
+        if field in ALLOWED_FIELDS:
+            setattr(song, field, value)
+
+    await song.save()
+    return song
+    
