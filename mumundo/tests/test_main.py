1748dff1984acd36b1cb5206af4c20b1191f7062
kpiaszynski <kpiaszynski@uiowa.edu>
2025-05-08 11:33:16 -0500

    Update test_main.py

--- mumundo/tests/test_main.py
+++ mumundo/tests/test_main.py
@@ -1,11 +1,12 @@
 import pytest
 from httpx import AsyncClient
-from main import app
-from backend.models.user import User
+from mumundo.main import app
 
 @pytest.mark.asyncio
 async def test_read_root():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.get("/")
+    from fastapi.testclient import TestClient
+    
+    client = TestClient(app)
+    response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Welcome to the Mumundo API!"}
+    assert response.json() == {"message": "Welcome to the Mumundo API! Visit /music to see the data! Enjoy!"}
